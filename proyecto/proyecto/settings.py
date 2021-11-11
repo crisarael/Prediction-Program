f89f5ecc6583b89300151b4adfa41c814e295efc
nicolas2589 <46973275+nicolas2589@users.noreply.github.com>
2021-11-10 18:47:25 -0800

    Update hosts

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c4=ny5u^!ec^w^9u0!hs65rr#q6lb0l^m(er9_)&h*^8%ndgy7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [0.0.0.0]
 
 
 # Application definition
